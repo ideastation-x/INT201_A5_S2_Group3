048907646e497a7e9bab49434624ae27e5ad2cac
ideastation-x <chumphu.phumin@gmail.com>
2021-10-17 18:14:21 +0700

    update comment

--- calTax.js
+++ calTax.js
@@ -1,14 +1,19 @@
-
+// * Import "function getPrice(priceList)" ด้วยชื่อ getPrice จาก ./getPrice.js เพื่อเอามาใช้ภายใน script นี้
 import { getPrice } from './getPrice.js'
 
+// * สร้าง TestCase เพื่อทดสอบ
 let priceList = { 
     testCase1: [10, 20, 30, 40, 50], 
     testCase2: [50, 300, 80, 100, 460], 
     testCase3: [168, 1890, 1987, 1237, 1984]
 }
+// * กำหนดภาษี
 let tax = 0.07
 
+// * เรียกใช้งาน Method getPrice แล้ว Assign ค่าให้ตัวแปรเพื่อให้สามารถเรียกใช้ Inner Function ผ่านตัวแปรได้
 let calTotalPriceWithTax_case1 = getPrice(priceList.testCase1)
+
+// * เรียกใช้งาน calTotalPriceWithTax_case1(tax) เพื่อค่านวณภาษีแล้วหาผลรวมของรายการสินค้าที่รวมภาษีแล้ว
 console.log(`Price List : ${priceList.testCase1}\nTax : ${(tax * 100).toFixed(0)}%\nTotal Price : ${calTotalPriceWithTax_case1(tax)}`)
 
 let calTotalPriceWithTax_case2 = getPrice(priceList.testCase2)
--- getPrice.js
+++ getPrice.js
@@ -1,7 +1,17 @@
+// * Export "function getPrice(priceList)" เป็น Module เพื่อสามารถเอาไปใช้ใน script อื่น ๆ ได้
+// * getPrice(priceList) เป็น function สำหรับรับค่ารายการสินค้าที่ยังไม่รวมภาษี 
 export function getPrice(priceList) {
-    function calculateTax(tax) {
-        const sumPriceWithTax = (previousValue, currentValue) => previousValue + currentValue;
-        return priceList.map((price) => price + (price * tax)).reduce(sumPriceWithTax)
+    
+    // * calculateTax(tax) เป็น function คำนวณภาษีแต่ละสินค้าชิ้น
+    // * แล้ว return เป็นผลรวมของรายการสินค้าที่รวมภาษีแล้ว
+    function calculateTax(tax) { 
+        
+        // * sumPriceWithTax เป็นการรับค่าก่อนหน้า, และค่าปัจจุบัน แล้ว return ออกมาเป็นผลรวมของทั้งสองค่า
+        // * ใช้ reduce โดยรับ parameter เป็น arrow function ที่รับค่าก่อนหน้า, และค่าปัจจุบัน แล้ว return ออกมาเป็นผลรวมของทั้งสองค่า
+        // * กับ priceList เพื่อบวกค่าหาผลรวมทั้งหมด
+        return priceList.map((price) => price + (price * tax)).reduce((previousValue, currentValue) => previousValue + currentValue)
     }
+    
+    // * return มาเป็นค่าผลรวมราคาของแต่ละชิ้นทั้งหมดที่รวมภาษีแล้ว
     return calculateTax
 }
