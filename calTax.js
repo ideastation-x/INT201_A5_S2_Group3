95148e451696c9bfd481004811e733054d252609
ideastation-x <chumphu.phumin@gmail.com>
2021-10-17 17:41:05 +0700

    update output

--- calTax.js
+++ calTax.js
@@ -1,7 +1,18 @@
+
 import { getPrice } from './getPrice.js'
-let priceList = prompt(`Enter your price list with format\n Ex. 10 20 30 40 50`)
-let getPriceList = (priceList.split(' ')).map(i => Number(i))
-console.log(getPriceList);
-let calTotalPriceWithTax = getPrice(getPriceList)
+
+let priceList = { 
+    testCase1: [10, 20, 30, 40, 50], 
+    testCase2: [50, 300, 80, 100, 460], 
+    testCase3: [168, 1890, 1987, 1237, 1984]
+}
 let tax = 0.07
-console.log(`Tax : ${(tax*100).toFixed(0)}%\nTotal Price : ${calTotalPriceWithTax(tax)}`)
+
+let calTotalPriceWithTax_case1 = getPrice(priceList.testCase1)
+console.log(`Price List : ${priceList.testCase1}\nTax : ${(tax * 100).toFixed(0)}%\nTotal Price : ${calTotalPriceWithTax_case1(tax)}`)
+
+let calTotalPriceWithTax_case2 = getPrice(priceList.testCase2)
+console.log(`Price List : ${priceList.testCase2}\nTax : ${(tax * 100).toFixed(0)}%\nTotal Price : ${calTotalPriceWithTax_case2(tax)}`)
+
+let calTotalPriceWithTax_case3 = getPrice(priceList.testCase3)
+console.log(`Price List : ${priceList.testCase3}\nTax : ${(tax * 100).toFixed(0)}%\nTotal Price : ${calTotalPriceWithTax_case3(tax)}`)
