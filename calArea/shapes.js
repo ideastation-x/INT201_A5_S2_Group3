f32242f9943593e7060686f7fc27d11e1f9a3e9b
Benjamin <bbenjmn01@gmail.com>
2021-10-20 18:46:00 +0700

    [update] shape.js

--- calArea/shapes.js
+++ calArea/shapes.js
@@ -1,5 +1,5 @@
 
-// * Function ในการคำนวณพื้นที่ของวงกลม
+// * Function ในการคำนวณพื้นที่ของรูปต่าง ๆ
 export function area(fn, n1, n2 ) {
     // * return ผลลัพธ์​ของ function กลับไปยัง area โดย Function จะได้รับมาจาก Paremeter -> fn ของ area และส่งค่า n1,n2 ไปยัง fn ที่ได้รับมา
     return fn(n1, n2);
@@ -8,6 +8,7 @@ export function area(fn, n1, n2 ) {
 // * Function ในการคำนวณพื้นที่ของวงกลม
 export function circle(n1, n2 = 0) {
     // * return มาเป็นพื้นที่ของวงกลม
+    // * กำหนด defualt parameter เนื่องจากการคำนวณพื้นที่วงกลมใส่ค่าเข้ามาเพียงค่าเดียว เพื่อไม่ให้เกิด error จากการใส่ค่าไม่ครบ
     return Math.PI * n1 * n1;
 }
 
