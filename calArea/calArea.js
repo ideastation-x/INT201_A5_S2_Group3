ea93f84f70fb19c7d9be3eee192d0d3c0f6ad3e7
ideastation-x <chumphu.phumin@gmail.com>
2021-10-20 18:39:10 +0700

    update comment calArea

--- calArea/calArea.js
+++ calArea/calArea.js
@@ -1,8 +1,12 @@
+// * Import "area, circle, triangle, rectangle" จาก ./shapes.js เพื่อเอามาใช้ภายใน script นี้
 import { area, circle, triangle, rectangle } from "./shapes.js";
 
+// * เรียกใช้งาน Method area แล้วส่ง Function การทำงานที่ได้สร้างไว้ผ่าน Parameter ของ area
 let shape1 = area(circle, 10);
 let shape2 = area(triangle, 3, 4);
 let shape3 = area(rectangle, 25, 25);
+
+// * แสดงผลลัพธ์ที่ return มาจาก area
 console.log(`shape1: ${shape1}`);
 console.log(`shape2: ${shape2}`);
 console.log(`shape3: ${shape3}`);
--- calArea/shapes.js
+++ calArea/shapes.js
@@ -1,15 +1,24 @@
+
+// * Function ในการคำนวณพื้นที่ของวงกลม
 export function area(fn, n1, n2 ) {
+    // * return ผลลัพธ์​ของ function กลับไปยัง area โดย Function จะได้รับมาจาก Paremeter -> fn ของ area และส่งค่า n1,n2 ไปยัง fn ที่ได้รับมา
     return fn(n1, n2);
 }
 
+// * Function ในการคำนวณพื้นที่ของวงกลม
 export function circle(n1, n2 = 0) {
+    // * return มาเป็นพื้นที่ของวงกลม
     return Math.PI * n1 * n1;
 }
 
+// * Function ในการคำนวณพื้นที่ของสามเหลี่ยม
 export function triangle(n1, n2) {
+    // * return มาเป็นพื้นที่ของสามเหลี่ยม
     return 0.5 * n1 * n2;
 }
 
+// * Function ในการคำนวณพื้นที่ของสี่เหลี่ยม
 export function rectangle(n1, n2) {
+    // * return มาเป็นพื้นที่ของสี่เหลี่ยม
     return n1 * n2;
 }
